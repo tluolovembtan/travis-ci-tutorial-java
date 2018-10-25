9bd731f59481c6f2b0fa2d9527fdf4542911a62b
luotao <tluolovebruce@gmail.com>
2018-10-25 16:37:32 +0800

    add

---
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/Test.java
@@ -0,0 +1,14 @@
+package io.github.joaomlneto.travis_ci_tutorial_java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: luotao
+ * Date: 2018/10/25
+ * Time: 下午4:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public int add(int a, int b){
+        return a+b;
+    }
+}
